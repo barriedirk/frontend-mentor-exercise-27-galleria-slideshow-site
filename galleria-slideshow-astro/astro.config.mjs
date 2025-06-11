03ef4b771a1b43ce1ff351672f407950c0af50cc
Barrie Freyre <barriedirk@yahoo.com>
2025-06-10 23:16:58 -0500

    add styles and js for slide

--- galleria-slideshow-astro/astro.config.mjs
+++ galleria-slideshow-astro/astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+const isDev = process.env.NODE_ENV === "development";
+
 // https://astro.build/config
 export default defineConfig({
+  // base: isDev ? "/" : "/frontend-mentor-exercise-27-galleria-slideshow-site/",
+  base: import.meta.env.PUBLIC_BASE || "/",
+
   vite: {
     plugins: [tailwindcss()],
   },
