4e67ee23254de8880dfe7cdabbd376556f0a4793
Barrie Freyre <barriedirk@yahoo.com>
2025-06-11 23:21:32 -0500

    generate dist

---
+++ docs/_astro/BaseLayout.astro_astro_type_script_index_1_lang.CBxl0pOq.js
@@ -0,0 +1,1 @@
+import"https://unpkg.com/dialog-polyfill@0.5.6/dist/dialog-polyfill.js";
--- galleria-slideshow-astro/astro.config.mjs
+++ galleria-slideshow-astro/astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-const isDev = process.env.NODE_ENV === "development";
+// const isDev = process.env.NODE_ENV === "development";
 
 // https://astro.build/config
 export default defineConfig({
   // base: isDev ? "/" : "/frontend-mentor-exercise-27-galleria-slideshow-site/",
-  base: import.meta.env.PUBLIC_BASE || "/",
+  // base: import.meta.env.PUBLIC_BASE || "/",
+
+  // site: "https://barriedirk.github.io",
+  // base: "frontend-mentor-exercise-27-galleria-slideshow-site",
 
   vite: {
     plugins: [tailwindcss()],
