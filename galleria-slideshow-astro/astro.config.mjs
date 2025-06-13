75d25adeaa72d816a19b290f77f1959eeb72cc18
Barrie Freyre <barriedirk@yahoo.com>
2025-06-12 19:15:06 -0500

    update href for github

--- galleria-slideshow-astro/astro.config.mjs
+++ galleria-slideshow-astro/astro.config.mjs
@@ -2,15 +2,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === "development";
+
+// console.log({ isDev });
 
 // https://astro.build/config
 export default defineConfig({
   // base: isDev ? "/" : "/frontend-mentor-exercise-27-galleria-slideshow-site/",
   // base: import.meta.env.PUBLIC_BASE || "/",
 
-  // site: "https://barriedirk.github.io",
-  // base: "frontend-mentor-exercise-27-galleria-slideshow-site",
+  site: isDev ? "" : "https://barriedirk.github.io",
+  base: isDev ? "/" : "frontend-mentor-exercise-27-galleria-slideshow-site",
+
   compressHTML: false,
   build: {
     inlineStylesheets: "always",
